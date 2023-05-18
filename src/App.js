5059f4b76de643f737497c94adacd053181b98a4
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2023-05-18 12:24:06 -0300

    Basic collapsible component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Collapsible from "./Collapsible";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Collapsible label="This is the label">
+                <h1>This is collapsible!</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nunc quis nibh ut ligula bibendum euismod. Duis tempor vulputate
+                    erat ac lacinia. Curabitur bibendum maximus ultrices. Maecenas felis arcu,
+                    dictum condimentum mi ut, interdum mattis magna. Quisque blandit faucibus lacus sit amet egestas.
+                    Aliquam convallis convallis ex, tincidunt facilisis ante porttitor nec.
+                    Morbi volutpat neque rutrum dui convallis imperdiet.
+                </p>
+            </Collapsible>
+            <Collapsible label="Click me">
+                <ul>
+                    <li>Some stuff</li>
+                    <li>Some stuff</li>
+                    <li>Some stuff</li>
+                    <li>Some stuff</li>
+                    <li>Some stuff</li>
+                    <li>Some stuff</li>
+                </ul>
+            </Collapsible>
+            <Collapsible label="😎">
+                <form>
+                    <input type="datetime-local"/>
+                    <button>Submit</button>
+                </form>
+            </Collapsible>
+        </div>
+    );
 }
 
 export default App;
